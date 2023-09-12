de24629bbbf76b33387b6188a331b6c56fe9ffd5
Emily <nemily1710@gmail.com>
2023-09-12 17:47:17 -0500

    feat: :sparkles: Consultas a la base de datos
    Se realizan las consultas tentativamente finales para el usuario

---
+++ src/collections/users.js
@@ -0,0 +1,51 @@
+import { connection } from "../utils/connect.js";
+
+class Users {
+  username;
+  password;
+  email;
+  fechaNacimiento;
+  constructor() {}
+  async connect() {
+    try {
+      const result = await connection("users");
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async searchUser() {
+    try {
+      const connection = await this.connect();
+      const resultado = await connection.findOne({ username: this.username });
+      console.log(resultado);
+      return resultado;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async searchEmail() {
+    try {
+      const connection = await this.connect();
+      const resultado = await connection.findOne({ email: this.email });
+      return resultado;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async postUser() {
+    try {
+      const connection = await this.connect();
+      const resultado = await connection.insertOne({
+        username: this.username,
+        password: this.password,
+        email: this.email,
+        fechaNacimiento: new Date(this.fechaNacimiento),
+      });
+      return resultado;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+export { Users };
