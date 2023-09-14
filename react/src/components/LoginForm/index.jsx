2dc08a3283651124c4e624743046bcbc32ffdad2
Emily <nemily1710@gmail.com>
2023-09-14 08:12:43 -0500

    feat: :sparkles: Component LoginForm

---
+++ react/src/components/LoginForm/index.jsx
@@ -0,0 +1,10 @@
+function LoginForm() {
+  return (
+    <>
+      <input placeholder="Usuario" />
+      <input placeholder="Contraseña" />
+      <button>Iniciar sesión</button>
+    </>
+  );
+}
+export { LoginForm };
