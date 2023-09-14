830b7776836130fc52bb5f87f0a1f024f917c265
Emily <nemily1710@gmail.com>
2023-09-14 08:09:00 -0500

    feat: :sparkles: Componentes principales pages
    Se realizan componentes principales donde irán cargadas las interfaces

---
+++ react/src/pages/App.jsx
@@ -0,0 +1,7 @@
+import { Login } from "./Login";
+import "./index.css";
+
+function App() {
+  return <Login className="container"></Login>;
+}
+export { App };
---
+++ react/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import { ContainerButtons } from "../components/ContainerButtons";
+import { FormContainer } from "../components/FormContainer";
+
+function Login() {
+  return (
+    <>
+      <ContainerButtons></ContainerButtons>
+      <FormContainer></FormContainer>
+    </>
+  );
+}
+export { Login };
