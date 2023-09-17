8d73336ad72be982fa1fd1ab3f63cf2de5d55edc
Emily <nemily1710@gmail.com>
2023-09-17 16:45:40 -0500

    fix: :ambulance: Cors
    Se solucionó problema de cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { initAPIRoutes } from "./routes/routes.js";
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json()); //! Middleaware para leer json
+app.use(cors())
 app.use("/api", initAPIRoutes());
 
 export default app;
